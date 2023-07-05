37e2abe4da61e8fe6ea6ab6257f2c6e19eaa8ff7
Michael Mrowetz <michael.mrowetz@rangle.io>
2023-07-04 23:27:30 -0400

    basics working

--- code.ts
+++ code.ts
@@ -7,28 +7,202 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { height: 500, width: 350, themeColors: true });
+
+const getPage = (node: BaseNode): PageNode | undefined => {
+  if (node.parent?.type === "PAGE") {
+    return node.parent;
+  }
+  return node.parent ? getPage(node.parent) : undefined;
+};
+
+type PageFinds = { page: PageNode; instances: InstanceNode[] };
+const findMissingLibraryComponentUsages = (componentNames: string[]) => {
+  return (
+    figma.root.findAll(
+      (n) =>
+        n.type === "INSTANCE" &&
+        !!n.mainComponent?.name &&
+        componentNames.includes(n.mainComponent?.name)
+    ) as InstanceNode[]
+  )
+    .map((f): [PageNode | undefined, InstanceNode] => [getPage(f), f])
+    .reduce<Record<string, PageFinds>>((acc, [page, find]) => {
+      if (!page) {
+        return acc;
+      }
+      if (acc[page.name]) {
+        acc[page.name].instances.push(find);
+        return acc;
+      }
+      return {
+        ...acc,
+        [page.name]: {
+          page,
+          instances: [find],
+        },
+      };
+    }, {});
+};
+
+type StyledNode = FrameNode | ComponentNode | InstanceNode;
+type PageStyleFinds = {
+  page: PageNode;
+  instances: Array<[string, StyledNode | TextNode]>;
+};
+const findMissingLibraryStyleUsages = (styleNames: string[]) => {
+  const stylesProps = [
+    "backgroundStyleId",
+    "effectStyleId",
+    "fillStyleId",
+    "gridStyleId",
+    "strokeStyleId",
+  ] as const;
+  type StyleProps = (typeof stylesProps)[number];
+
+  const isMissingStyle = (node: StyledNode, prop: StyleProps) => {
+    if (prop in node) {
+      const styleName = figma.getStyleById(node[prop].toString())?.name;
+      return styleNames.includes(styleName ?? "") ? styleName : undefined;
+    }
+  };
+
+  let styledNodes = figma.root.findAll(
+    (n) =>
+      stylesProps.every((v) => v in n) &&
+      stylesProps.some((v) => (n as StyledNode)[v] !== "")
+  ) as Array<StyledNode>;
+
+  const missingFrameAndInstanceStyles = styledNodes.flatMap((node) => {
+    return stylesProps.flatMap<[PageNode | undefined, string, StyledNode]>(
+      (style) => {
+        const mayBeStyleName = isMissingStyle(node, style);
+        if (mayBeStyleName) {
+          return [[getPage(node), mayBeStyleName, node]];
+        }
+        return [];
+      }
+    );
+  });
+
+  let textNodes = figma.root.findAll(
+    (node) => "textStyleId" in node && node.textStyleId !== ""
+  ) as TextNode[];
+
+  const missingTextNodeStyle = textNodes.flatMap<
+    [PageNode | undefined, string, TextNode]
+  >((node) => {
+    const textStyleId = figma.getStyleById(node.textStyleId.toString())?.name;
+    if (textStyleId && styleNames.includes(textStyleId)) {
+      return [[getPage(node), textStyleId, node]];
+    }
+    return [];
+  });
+
+  const allStylesByName = [
+    ...missingFrameAndInstanceStyles,
+    ...missingTextNodeStyle,
+  ].reduce<Record<string, PageStyleFinds>>((acc, [page, styleName, node]) => {
+    if (!page) {
+      return acc;
+    }
+    if (acc[page.name]) {
+      acc[page.name].instances = [
+        ...acc[page.name].instances,
+        [styleName, node],
+      ];
+      return acc;
+    }
+    return {
+      ...acc,
+      [page.name]: {
+        page,
+        instances: [[styleName, node]],
+      },
+    };
+  }, {});
+
+  return allStylesByName;
+};
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = (msg, props) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+  if (msg.type === "find-missing-library-usage") {
+    console.log("searching...");
+    const nodes = findMissingLibraryComponentUsages(msg.search);
+    const styleUsages = findMissingLibraryStyleUsages(msg.search);
+
+    // console.log(styleUsages);
+    let containsCurrentPage = false;
+    Object.values(nodes).forEach(({ page, instances }) => {
+      // select
+      page.selection = instances;
+      if (figma.currentPage === page) {
+        figma.viewport.scrollAndZoomIntoView(instances);
+        containsCurrentPage = true;
+      }
+    });
+
+    if (!containsCurrentPage) {
+      const { page, instances } = Object.values(nodes)[0];
+      figma.currentPage = page;
+      figma.viewport.scrollAndZoomIntoView(instances);
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+
+    console.log("styleUsages", styleUsages);
+    let result: Array<
+      [
+        { pageId: string; pageName: string },
+        Array<{ id: string; name: string; styleName?: string }>
+      ]
+    > = Object.entries(nodes).map(([pageName, { page, instances }]) => {
+      return [
+        {
+          pageId: page.id,
+          pageName: page.name,
+        },
+        instances.map((i) => ({
+          id: i.id,
+          name: i.name,
+        })),
+      ];
+    });
+
+    Object.entries(styleUsages).forEach(([pageName, { page, instances }]) => {
+      result.push([
+        {
+          pageId: page.id,
+          pageName: page.name,
+        },
+        instances.map(([styleName, i]) => ({
+          id: i.id,
+          name: i.name,
+          styleName,
+        })),
+      ]);
+    });
+
+    console.log(">>> result", result);
+
+    figma.ui.postMessage({ type: "result", nodes: result });
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  if (msg.type === "focus-instance") {
+    const { pageId, instanceId } = msg;
+    const page = figma.getNodeById(pageId) as PageNode;
+    const node = figma.getNodeById(instanceId) as SceneNode;
+
+    page.selection = [node];
+    figma.currentPage = page;
+    figma.viewport.scrollAndZoomIntoView([node]);
+  }
+  if (msg.type === "cancel") {
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
+  }
 };
